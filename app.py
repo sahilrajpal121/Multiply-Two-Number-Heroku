2fe84576308cfce31629f4f1856ca3b560219ca6
Sahil Rajpal <40625432+sahilrajpal121@users.noreply.github.com>
2022-07-24 18:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 import streamlit as st
 
+string = "Multiplier"
+st.set_page_config(page_title=string, page_icon="🧮")
+
 st.write("""
 # Two Numbler Multiplication App
 
 This app multiplies two number
 """)
 
+
 st.header('User Input Parameters')
 
 def user_input_features():
-    n1 = st.number_input("Number_1")
-    n2 = st.number_input("Number_2")
+    n1 = st.number_input("Enter Number-1")
+    n2 = st.number_input("Enter Number-2")
 
     return n1, n2
 
 n1, n2 = user_input_features()
-st.write(int(n1*n2))
+
+if st.button('Calculate'):
+    st.write(n1*n2)
